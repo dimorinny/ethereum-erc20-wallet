5778e1d882184047f099584576bd04d4c90dec4f
dmerkurev <dmerkurev@avito.ru>
2017-06-28 01:38:42 +0300

    add semantic + search screen markup

--- src/containers/input/Input.js
+++ src/containers/input/Input.js
@@ -1,15 +1,47 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actionCreators from '../../actions/token';
+import {Icon, Input, Form} from 'semantic-ui-react';
+import metamaskPath from '../../../static/img/metamask.png';
+import './input.css';
 
+@connect(mapStateToProps, mapDispatchToProps)
 export default class InputPage extends Component {
 
     static propTypes = {};
 
     render() {
+        console.log(this.props);
+
         return (
-            <div>
-                Search here
+            <div className='input_container'>
+
+                <div className='input_header_container'>
+                    <img className='input_header_image' src={metamaskPath}/>
+                </div>
+
+                <Form onSubmit={() => this.props.router.push("qwe")}>
+                    <Form.Field>
+                        <Input
+                            size='massive'
+                            placeholder='0x1fDE9bAf52bBa2Ae3CC019FeD9d0C779D16553ed'
+                            className='input_field'
+                        />
+                    </Form.Field>
+                </Form>
             </div>
         );
+    };
+}
+
+function mapStateToProps(state) {
+    return {};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(actionCreators, dispatch)
     };
 }
