44369fdce6f242096fbabf68ed16d04378260d66
dmerkurev <dmerkurev@avito.ru>
2017-07-02 18:37:35 +0300

    account markup

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
+import {Grid, Segment, Button, Icon} from 'semantic-ui-react';
 import './account.css';
 
 export default class Account extends Component {
@@ -24,13 +25,36 @@ export default class Account extends Component {
         } = this.props.account;
 
         return (
-            <div className='account_address'>
-                <div>Address: {address}</div>
-                <div>Contract Address: {contractAddress}</div>
-                <div>Balance: {balance.toNumber()}</div>
-                <div>Symbol: {symbol}</div>
-                <div>TotalSupply: {totalSupply.toFormat(0)}</div>
-            </div>
+            <Grid columns='equal'>
+                <Grid.Column/>
+                <Grid.Column width={14}>
+                    <Segment className='account_container'>
+                        <Grid columns={2} divided>
+                            <Grid.Row>
+                                <Grid.Column>
+                                    <div>Address: {address}</div>
+                                    <div>Balance: {balance.toNumber()}</div>
+                                    <div>
+                                        <Button
+                                            basic
+                                            disabled={balance.toNumber() === 0}
+                                            className='account_send_button'
+                                            color='black'>
+                                            <Icon name='send'/> Send Tokens
+                                        </Button>
+                                    </div>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <div>Contract Address: {contractAddress}</div>
+                                    <div>Symbol: {symbol}</div>
+                                    <div>TotalSupply: {totalSupply.toFormat(0)}</div>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment>
+                </Grid.Column>
+                <Grid.Column/>
+            </Grid>
         );
     };
 }
